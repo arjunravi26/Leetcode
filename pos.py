b55216627e793230f75b1ead76ec60b666f5631f
Arjun Ravi <121337838+arjunravi26@users.noreply.github.com>
2024-08-21 20:58:52 +0530

    Search Insert Position

---
+++ pos.py
@@ -0,0 +1,17 @@
+# 35. Search Insert Position
+# Solved
+# Easy
+# Topics
+# Companies
+# Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+# You must write an algorithm with O(log n) runtime complexity.
+from typing import List
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        for index,num in enumerate(nums):
+            if num == target or num > target:
+                return index
+        return index+1
